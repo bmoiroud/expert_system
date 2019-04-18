4de21892dbea4992bfa2a838c60225c494a9f2a5
Emmanuel FERRANDINI <eferrand@student.42.fr>
2019-04-18 16:48:55 +0200

    correc_test ajouté

---
+++ correct_script.sh
@@ -0,0 +1,38 @@
+# faire tous les tests
+declare -i nb=1
+string=tests/test
+make re
+clear
+
+while ((nb <= 29))
+do
+	echo "##################################################"
+	echo ""
+	final=$string$nb
+	cat $string$nb
+	./expert-system $final
+	((nb++))
+	read  -n 1
+done
+
+
+echo "##################################################"
+echo ""
+echo "tests de <=>"
+cat tests/test_double_inclusion
+./expert-system tests/test_double_inclusion
+read  -n 1
+
+echo "##################################################"
+echo ""
+echo "test incoherence input"
+cat tests/test_incoherence
+./expert-system tests/test_double_inclusion
+read  -n 1
+
+echo "##################################################"
+echo ""
+echo "test cas indetermined"
+cat tests/test_indetermined
+./expert-system tests/test_double_inclusion
+read  -n 1
